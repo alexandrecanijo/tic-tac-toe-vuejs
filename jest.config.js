21c1f4340b7dd7e30f2d24767bef15f8df7b529e
Alexandre Canijo <alex@alexandrecanijo.com>
2018-11-10 22:51:54 +0000

    Setting up TravisCI

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  moduleFileExtensions: ["js", "jsx", "json", "vue"],
+  moduleFileExtensions: ["js", "jsx", "json", "vue", "scss"],
   transform: {
     "^.+\\.vue$": "vue-jest",
     ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
@@ -13,5 +13,26 @@ module.exports = {
   testMatch: [
     "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
   ],
-  testURL: "http://localhost/"
+  testURL: "http://localhost/",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,vue}",
+    "!src/components/**/index.js",
+    "!**/node_modules/**",
+    "!**/vendor/**",
+    "!src/*/main.js"
+  ],
+  coverageDirectory: "<rootDir>/coverage",
+  coverageReporters: [
+    "json",
+    "lcov"
+  ],
+  coverageThreshold: {
+    "global": {
+      "branches": 90,
+      "functions": 90,
+      "lines": 90,
+      "statements": 90
+    }
+  }
 };
