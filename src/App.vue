17e5f7e3688445fe37b0e0cf19051f3b10438960
Alexandre Canijo <alex@alexandrecanijo.com>
2018-11-12 01:45:53 +0000

    Adding new themes

--- src/App.vue
+++ src/App.vue
@@ -16,14 +16,6 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Arial, Tahoma, Verdana, sans-serif;
-  font-size: 14px;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #000;
-  margin: 0;
-  padding: 0;
-  border: 0;
-}
+@import "styles/index";
+@import "styles/base-theme/index";
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,22 @@
 // vue.config.js file to be place in the root of your repository
 // make sure you update `yourProjectName` with the name of your GitLab project
+const path = require('path');
 
 module.exports = {
-  baseUrl: process.env.NODE_ENV === "production" ? "/tictactoevuejs/" : "/"
+  baseUrl: process.env.NODE_ENV === "production" ? "/tictactoevuejs/" : "/",
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+    types.forEach(type => addStyleResource(config.module.rule('sass').oneOf(type)));
+  }
 };
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/styles/index.scss'),
+        path.resolve(__dirname, './src/styles/base-theme/index.scss')
+      ],
+    })
+}
